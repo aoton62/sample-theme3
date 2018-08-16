61367a1700ec12bd0a04a10dd5bd1c21a69a340d
aoton62 <aotoiwt@gmail.com>
2018-08-16 21:08:55 +0900

    header.phpの作成

---
+++ header.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <header id="top">
+    <h1>
+      <a href="index.html"><img src="images/logo.gif" alt="日本の景色写真展" width="258" height="64"></a>
+    </h1>
+  </header>
+  <nav>
+    <ul>
+      <li class="nav_introduction"><a href="introduction.html">展覧会について</a></li>
+      <li class="nav_schedule"><a href="schedule.html">スケジュール</a></li>
+      <li class="nav_gallery"><a href="gallery.html">作品紹介</a></li>
+      <li class="nav_tickets"><a href="tickets.html">前売り券申し込み</a></li>
+    </ul>
+  </nav>
+
+
+
+</body>
+</html>
